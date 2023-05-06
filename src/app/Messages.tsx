d14087d1577d9fee1ac73818dbb76a8880372da7
2dayevents <simone.priuli@icloud.com>
2023-05-06 14:36:35 +0200

    update max width on messages

--- src/app/Messages.tsx
+++ src/app/Messages.tsx
@@ -1,6 +1,6 @@
 export default function Messages(props: { messages: string[] }) {
   return (
-    <div className="flex flex-col gap-2 mt-4 max-w-md">
+    <div className="flex flex-col gap-2 mt-4 max-w-sm">
       {props.messages.map((message) => {
         return (
           <div key={message} className="p-3 bg-slate-300 rounded-full">
