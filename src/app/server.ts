ebee7e7c05895b52054ffb4da1348a49c0dae52d
2dayevents <simone.priuli@icloud.com>
2023-05-06 14:27:03 +0200

    add randommessage function

--- src/app/server.ts
+++ src/app/server.ts
@@ -4,11 +4,18 @@ import { z } from "zod";
 import { zact } from "zact/server";
 import { redis } from "@/database/redis";
 import { revalidatePath } from "next/cache";
+import { defaultMessages } from "./defaultmessages";
 
 const randomstring = require("randomstring");
 
+function RandomMessage() {
+  const randomIndex = Math.floor(Math.random() * defaultMessages.length);
+  const randomMessage = defaultMessages[randomIndex];
+  return randomMessage;
+}
+
 export async function AddMessage() {
   const key: string = randomstring.generate();
-  await redis.set(key, "Hello World");
+  await redis.set(key, RandomMessage());
   revalidatePath("/");
 }
